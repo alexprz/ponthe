1b8167dda67394ba64307d703d7adc1d8095780f
MathieuLerouge91 <lerouge.mathieu91@gmail.com>
2018-12-02 11:01:34 +0100

    Formatting

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Navigation from './Navigation/Navigation'
+import Navigation from './navigation/Navigation'
 
 export default class App extends React.Component {
   render() {
