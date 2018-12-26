568753e5df9c4e318e23aad7ca7e41ee8c6f28bf
Mathieu Lerouge <lerouge.mathieu91@gmail.com>
2018-12-26 10:22:04 +0100

    Fix navigation between drawer and tab bar

--- components/Members.js
+++ components/Members.js
@@ -3,11 +3,6 @@ import { StyleSheet, View, Text } from 'react-native'
 
 class Members extends React.Component {
 
-  componentWillMount() {
-    //console.log(this.props.navigation)
-    //this.props.navigation.openDrawer()
-  }
-
   render() {
     return (
       <View style={styles.main_container}>
